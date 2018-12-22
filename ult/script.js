641d75a7ecb26f30c904fe1ff9620aaf8f7c97f3
Dmitriy <38003065+plusex@users.noreply.github.com>
2018-12-23 03:28:48 +0500

    Add files via upload

--- ult/script.js
+++ ult/script.js
@@ -5,42 +5,48 @@ window.addEventListener('load', function (e) {
         var tag = document.querySelector(tagClass);
         var text = tag.innerHTML;
         tag.innerHTML = '';
-        for (let i = 0; i < text.length; i++) {
-            var tagCreate = document.createElement('span');
-            tagCreate.innerHTML = text.charAt(i);
-            tag.append(tagCreate);
-            tagCreate.classList.add('wip');
-        }
-        textWriter()
-        function textWriter() {
-            var span = document.querySelectorAll('span.wip');
-            for (let i = 0; i < span.length; i++) {
-                span[i].addEventListener('mouseover', function () {
-                    var contex = this;
-                    contex.classList.add('active');
-                    setTimeout(function () {
-                        contex.classList.remove('active');
-                        contex.classList.add('activeAfter');
-                        setTimeout(function () {
-                            contex.classList.remove('activeAfter');
-                        }, 300)
-                    }, 300)
-                })
-                setTimeout(function () {
-                    span[i].style.display = 'inline-block';
-                    span[i].classList.add('active');
-                    span[i].style.color = '#FC0853';
-                    setTimeout(function () {
-                        span[i].style.color = '';
-                        span[i].classList.remove('active');
-                        span[i].classList.add('activeAfter');
-                        setTimeout(function () {
-                            span[i].classList.remove('activeAfter');
-                        }, 300)
-                    }, 20 * i)
-                }, 150 * i)
+        setTimeout(function () {
+            for (let i = 0; i < text.length; i++) {
+                var tagCreate = document.createElement('span');
+                tagCreate.innerHTML = text.charAt(i);
+                if(tagCreate.innerHTML == ' ') {
+                    tagCreate.innerHTML = '&nbsp'
+                }
+                tag.append(tagCreate);
+                tagCreate.classList.add('wip');
             }
-        }
+            textWriter()
+
+            function textWriter() {
+                var span = document.querySelectorAll('span.wip');
+                for (let i = 0; i < span.length; i++) {
+                    span[i].addEventListener('mouseover', function () {
+                        var contex = this;
+                        contex.classList.add('active');
+                        setTimeout(function () {
+                            contex.classList.remove('active');
+                            contex.classList.add('activeAfter');
+                            setTimeout(function () {
+                                contex.classList.remove('activeAfter');
+                            }, 300)
+                        }, 300)
+                    })
+                    setTimeout(function () {
+                        span[i].style.display = 'inline-block';
+                        span[i].classList.add('active');
+                        span[i].style.color = '#FC0853';
+                        setTimeout(function () {
+                            span[i].style.color = '';
+                            span[i].classList.remove('active');
+                            span[i].classList.add('activeAfter');
+                            setTimeout(function () {
+                                span[i].classList.remove('activeAfter');
+                            }, 300)
+                        }, 20 * i)
+                    }, 70 * i)
+                }
+            }
+        }, 500);
     }
 
 
